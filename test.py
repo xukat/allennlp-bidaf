a099650c0cca4dd467ade2a4bf30ec00dd954c29
xukat <xu.katie@hotmail.com>
2021-03-20 19:31:05 -0400

    cleaner

--- test.py
+++ test.py
@@ -26,128 +26,80 @@ from allennlp.data.data_loaders import SimpleDataLoader
 from allennlp.training.util import evaluate
 
 
-# pretrained_mdls = pretrained.get_pretrained_models()
-# print(pretrained_mdls)
+def download_data(data_dir, squad_ver):
+    train_data_filename = "train-v"+str(squad_ver)+".json"
+    dev_data_filename = "dev-"+str(squad_ver)+".json"
 
+    train_data_path = os.path.join(data_dir, train_data_filename)
+    dev_data_path = os.path.join(data_dir, dev_data_filename)
 
-data_dir = "data/"
-squad_ver=1.1
+    train_data_url = "https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v"+str(squad_ver)+".json"
+    dev_data_url = "https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v"+str(squad_ver)+".json"
 
-train_data_filename = "train-v"+str(squad_ver)+".json"
-dev_data_filename = "dev-"+str(squad_ver)+".json"
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
 
-train_data_path = os.path.join(data_dir, train_data_filename)
-dev_data_path = os.path.join(data_dir, dev_data_filename)
-
-train_data_url = "https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v"+str(squad_ver)+".json"
-dev_data_url = "https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v"+str(squad_ver)+".json"
-
-if not os.path.exists(data_dir):
-    os.makedirs(data_dir)
-
-if not os.path.exists(train_data_path):
-    print("Downloading train data...")
-    item = requests.get(train_data_url)
-    data = item.json()
-    with open(train_data_path, 'w') as f:
-            json.dump(data, f)
-else:
-    print("Train data already downloaded.")
-
-
-if not os.path.exists(dev_data_path):
-    print("Downloading dev data...")
-    item = requests.get(dev_data_url)
-    data = item.json()
-    with open(dev_data_path, 'w') as f:
-            json.dump(data, f)
-else:
-    print("Dev data already downloaded.")
-
-bidaf_pred = pretrained.load_predictor("rc-bidaf")
-
-token_indexers = {'token_characters': TokenCharactersIndexer(), 'tokens': SingleIdTokenIndexer()}
-if squad_ver==1.1:
-    dataset_reader = SquadReader.squad1(token_indexers=token_indexers)
-elif squad_ver==2.0:
-    dataset_reader = SquadReader.squad2(token_indexers=token_indexers)
-else:
-    raise
-
-instances = dataset_reader.read(dev_data_path)
-
-instance = []
-count = 0
-for i in instances:
-    if len(instance) < 2:
-        count += 1
-        if count == 8 or count == 123:
-            instance.append(i)
+    if not os.path.exists(train_data_path):
+        print("Downloading train data...")
+        item = requests.get(train_data_url)
+        data = item.json()
+        with open(train_data_path, 'w') as f:
+                json.dump(data, f)
     else:
-        break
-print(instance)
+        print("Train data already downloaded.")
 
-# pdb.set_trace()
-#for instance in instances[:10]:
-#        print(instance)
+    if not os.path.exists(dev_data_path):
+        print("Downloading dev data...")
+        item = requests.get(dev_data_url)
+        data = item.json()
+        with open(dev_data_path, 'w') as f:
+                json.dump(data, f)
+    else:
+        print("Dev data already downloaded.")
 
-bidaf_mdl = bidaf_pred._model
+    return train_data_path, dev_data_path
 
-from allennlp.data.batch import Batch
-
-dataset = Batch(instance)
-dataset.index_instances(bidaf_mdl.vocab)
-mdl_input = dataset.as_tensor_dict()
-
-output = bidaf_mdl(mdl_input['question'], mdl_input['passage'])
-# scratch_bidaf_mdl = BidirectionalAttentionFlow()
-
-# pdb.set_trace()
-
-
-def run_training_loop(model, dataset_reader, train_data_path, dev_data_path):
+def load_data(train_data_path, dev_data_path, squad_ver):
+    token_indexers = {'token_characters': TokenCharactersIndexer(), 'tokens': SingleIdTokenIndexer()}
+    if squad_ver==1.1:
+        dataset_reader = SquadReader.squad1(token_indexers=token_indexers)
+    elif squad_ver==2.0:
+        dataset_reader = SquadReader.squad2(token_indexers=token_indexers)
+    else:
+        raise
 
     print("Reading data...")
     tic = time.time()
+
     train_data = list(dataset_reader.read(train_data_path))
     dev_data = list(dataset_reader.read(dev_data_path))
+
     print("Time elapsed:", time.time()-tic)
 
-    train_loader, dev_loader = build_data_loaders(train_data, dev_data)
-    train_loader.index_with(model.vocab)
-    dev_loader.index_with(model.vocab)
+    return train_data, dev_data
 
-    # You obviously won't want to create a temporary file for your training
-    # results, but for execution in binder for this guide, we need to do this
-    serialization_dir = "tmp/"
-    trainer = build_trainer(model, serialization_dir, train_loader, dev_loader, num_epochs=1)
-    print("Starting training")
-    tic - time.time()
-    trainer.train()
-    print("Finished training")
-    print("Time elapsed:", time.time()-tic)
-
-    return model, dataset_reader
-
-
-def build_data_loaders(train_data: List[Instance], dev_data: List[Instance], batch_size=8) -> Tuple[DataLoader, DataLoader]:
+def build_data_loaders(train_data: List[Instance],
+                       dev_data: List[Instance],
+                       batch_size
+                      ) -> Tuple[DataLoader, DataLoader]:
 
     train_loader = SimpleDataLoader(train_data, batch_size, shuffle=True)
     dev_loader = SimpleDataLoader(dev_data, batch_size, shuffle=False)
 
     return train_loader, dev_loader
 
-
 def build_trainer(
     model: Model,
     serialization_dir: str,
     train_loader: DataLoader,
     dev_loader: DataLoader,
-    num_epochs: int
+    num_epochs: int,
+    learning_rate: float = 0.001,
+    cuda_device = None
 ) -> Trainer:
 
     parameters = [(n, p) for n, p in model.named_parameters() if p.requires_grad]
-    optimizer = AdamOptimizer(parameters)  # type: ignore
+    optimizer = AdamOptimizer(parameters, lr=learning_rate)  # type: ignore
     trainer = GradientDescentTrainer(
         model=model,
         serialization_dir=serialization_dir,
@@ -155,23 +107,46 @@ def build_trainer(
         validation_data_loader=dev_loader,
         num_epochs=num_epochs,
         optimizer=optimizer,
+        cuda_device=cuda_device
     )
 
     return trainer
 
-# pdb.set_trace()
-run_training_loop(bidaf_mdl, dataset_reader, train_data_path, dev_data_path)
+data_dir = "data/"
+squad_ver=1.1
 
-# bidaf_pred._model = bidaf_mdl
+# download data and load
+train_data_path, dev_data_path = download_data(data_dir, squad_ver)
+train_data, dev_data = load_data(train_data_path, dev_data_path)
+train_loader, dev_loader = build_data_loaders(train_data, dev_data)
 
-dev_data = list(dataset_reader.read(dev_data_path))
-_, dev_loader = build_data_loaders(dev_data, dev_data)
+# load pretrained model
+bidaf_pred = pretrained.load_predictor("rc-bidaf")
+model = bidaf_pred._model
+
+# index model with vocab
+train_loader.index_with(bidaf_mdl.vocab)
 dev_loader.index_with(bidaf_mdl.vocab)
 
-results = evaluate(bidaf_mdl, dev_loader)
+# pdb.set_trace()
+
+save_dir = "tmp/"
+num_epochs = 1
+
+trainer = build_trainer(model, save_dir, train_loader, dev_loader, num_epochs)
+# trainer = build_trainer(model, save_dir, train_loader, dev_loader, num_epochs, learning_rate, cuda_device)
+
+print("Starting training")
+tic - time.time()
+
+trainer.train()
+
+print("Finished training")
+print("Time elapsed:", time.time()-tic)
+
+results = evaluate(model, dev_loader)
 
 # Pretrained model: start_acc: 0.30, end_acc: 0.31, span_acc: 0.20, em: 0.27, f1: 0.41, loss: 7.04 ||: : 1322it [05:46,  3.82it/s]
 # Trained one epoch: start_acc: 0.53, end_acc: 0.57, span_acc: 0.44, em: 0.53, f1: 0.65, loss: 3.39 ||: : 1322it [05:57,  3.70it/s]
-
 
 pdb.set_trace()
